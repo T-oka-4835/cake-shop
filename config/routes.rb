a3498d49f8171bb805783c933605fb6fdd8e51c3
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-31 12:40:06 +0000

    [Add]moduleでコントローラーを分割する準備を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
-devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
-}
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  scope module: :public do
+    get 'my_page' => 'customers#show'
+    get 'edit' => 'customers#edit'
+    patch 'customers' => 'customers#update'
+    get 'unsubscribe' => 'customers#unsubscribe'
+    patch 'withdraw' => 'customers#withdraw'
+    
+    
+  end
+
+
+
+
   root 'homes#top'
+  get 'about' => 'homes#about'
 end
