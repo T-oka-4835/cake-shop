ff9e61afb1ada85ffa9e398e026ace7247513e88
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-01-31 11:12:18 +0000

    [Delete]Adminモデルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -21,13 +21,6 @@ ActiveRecord::Schema.define(version: 2021_01_30_061147) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "carts", force: :cascade do |t|
     t.integer "item_id"
     t.integer "customer_id"
