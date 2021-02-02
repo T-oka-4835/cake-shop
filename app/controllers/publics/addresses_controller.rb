85be264c6d16cb49b02d5ab05631b477a699562c
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-02-02 08:49:18 +0000

    [Add]Addressの一覧を追加

--- app/controllers/publics/addresses_controller.rb
+++ app/controllers/publics/addresses_controller.rb
@@ -3,18 +3,27 @@ class Publics::AddressesController < ApplicationController
   def index
     @address = Address.new
     @addresses = Address.all
+    @customer = current_customer
   end
 
   def create
+    address = Address.new(address_params)
+    address.save
+    redirect_to addresses_path
   end
 
   def edit
   end
 
+  def update
+  end
+
   def destroy
   end
 
-  def update
+  private
+  def address_params
+    params.require(:address).permit(:postcode, :address, :name)
   end
 
 end
