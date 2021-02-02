e523913338c96e61b7c056c9a119915bd665ab4d
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-02-02 08:56:29 +0000

    [Add]Addressの編集機能の追加

--- app/controllers/publics/addresses_controller.rb
+++ app/controllers/publics/addresses_controller.rb
@@ -13,9 +13,13 @@ class Publics::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
