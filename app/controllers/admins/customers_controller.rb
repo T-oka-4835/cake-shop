da2e0beeb5a04b3fa0e9ccbaecd687f8f0cc4b48
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-02-05 08:25:38 +0000

    [Add]Admin側での会員の編集機能を実装

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -13,6 +13,14 @@ class Admins::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admins_customer_path(customer)
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :phonenumber, :withdraw)
   end
 
 end
