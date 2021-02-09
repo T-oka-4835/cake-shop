902d2352baf397843bac2dddb092ffbd432249ca
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-02-09 02:11:40 +0000

    [Fix]カートへと商品を追加できるよう編集

--- app/controllers/publics/items_controller.rb
+++ app/controllers/publics/items_controller.rb
@@ -7,6 +7,14 @@ class Publics::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart = Cart.new
+      if @cart.amount == '個数選択'
+        redirect_to item_path(item)
+      end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price, :image, :is_status)
   end
 
 end
