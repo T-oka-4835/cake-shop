c0b7ac6e90914c7096a3e8c86834015000699543
T-oka-4835 <okalovereal35@yahoo.co.jp>
2021-02-18 08:15:58 +0000

    [Add]order/cunfirmに記述を追加

--- app/controllers/publics/orders_controller.rb
+++ app/controllers/publics/orders_controller.rb
@@ -19,6 +19,21 @@ class Publics::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @customer = current_customer
     @carts = @customer.carts.all
+
+    if [:orders] == "ご自身の住所"
+      @order.postcode = @customer.postcode
+      @order.address = @order.address
+      @order.last_name + @order.first_name
+    elsif [:order] == "登録済住所から選択"
+      @order.postcode = @address.postcode
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      @order.postcode = Order.find_by(postcode: @order.postcode)
+      @order.address = Order.find_by(address: @order.address)
+      @order.name = Order.find_by(name: @order.name)
+    end
+
   end
 
   def complete
